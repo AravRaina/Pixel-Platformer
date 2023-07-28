d1c4ef0fb95c11bce456eb31cf394a6231b8eabf
AravRaina <127677155+AravRaina@users.noreply.github.com>
2023-07-28 10:18:42 -0400

    Add files via upload

---
+++ Platforms.js
@@ -0,0 +1,24 @@
+class Platform{
+    constructor(x, y, w, h, colour){
+        let options = {
+            isStatic:true
+        };
+        
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
+        this.colour = colour
+
+    }
+
+    show() {
+        var pos = this.body.position
+        fill(this.colour)
+        rectMode(CENTER)
+        rect(pos.x, pos.y, this.w, this.h)
+    
+    
+    }
+
+}
